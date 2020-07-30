cffd55ab410b4826c6e7f4f162a4f9c7dfdd21df
linikerj <linikerenem@gmail.com>
2020-07-30 00:41:29 -0300

    primeira sincronização do react

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path # path is a relative path to create a route
-from django.conf.urls.static import static # static provides a file static to me
 from django.conf import settings
-
-from users.views import UserProfileView
+from django.conf.urls.static import static # static provides a file static to me
+from django.conf.urls import url
+from users import views
 
 
 urlpatterns = [
-    path('', UserProfileView.as_view(), name='user_profile'),
-
+    url(r'^api/customers/$', views.customers_list),
+    url(r'^api/customers/(?P<pk>[0-9]+)$', views.customers_detail),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
