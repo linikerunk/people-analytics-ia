20417c5e4491ba2354eb31d17e5774d9da97a527
Liniker Oliveira <linikerenem@gmail.com>
2020-08-20 16:34:20 -0300

    ADD: .env in a project to hide the secret key and debug.

--- PeopleAnalytics/asgi.py
+++ PeopleAnalytics/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PeopleAnalyticsIA project.
+ASGI config for PeopleAnalytics project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleAnalyticsIA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleAnalytics.settings')
 
 application = get_asgi_application()
--- PeopleAnalytics/wsgi.py
+++ PeopleAnalytics/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PeopleAnalyticsIA project.
+WSGI config for PeopleAnalytics project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleAnalyticsIA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleAnalytics.settings')
 
 application = get_wsgi_application()
